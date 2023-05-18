3b5a350ced1838ebc1591e18db9321d60f966d25
Emilio Pino <pinoen@yahoo.com.ar>
2023-05-17 23:30:04 -0300

    exercise 2.9 the phonebook step4 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,22 +2,32 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState({
     name: '',
-    number: ''
+    number: '',
+    id: ''
   })
+
+  const [searchWord, setSearchWord] = useState('')
+
+  const handleSearch = (e) => {
+    setSearchWord(e.target.value)
+  }
+
+  const filteredArray = persons.filter(person => person.name.toLocaleLowerCase().includes(searchWord.toLowerCase()));
 
   const handleChange = (e) => {
     const { name, value } = e.target
     setNewName(prev => {
       return {
         ...prev,
-        [name]: value
+        [name]: value,
+        id: persons.length + 1
       }
     })
   }
@@ -26,16 +36,21 @@ const App = () => {
     e.preventDefault()
     if (persons.some(person => person.name === newName.name)) {
       alert(`${newName.name} is already added to phonebook`)
-      setNewName({ name: '', number: '' })
+      setNewName({ name: '', number: '', id: '' })
     } else {
       setPersons(persons.concat(newName))
-      setNewName({ name: '', number: '' })
+      setNewName({ name: '', number: '', id: '' })
     }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <span>Filter shown by</span><input value={searchWord} onChange={handleSearch} />
+      <br />
+      <br />
+
+      <h2>Add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName.name} onChange={handleChange} name='name' />
@@ -47,7 +62,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+      {searchWord ? filteredArray.map(person => <p key={person.id}>{person.name} {person.number}</p>) : persons.map(person => <p key={person.id}>{person.name} {person.number}</p>)}
     </div>
   )
 }
