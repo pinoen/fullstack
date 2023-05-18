44a7027c2e3805e88029be5172958d48bc7ae783
Emilio Pino <pinoen@yahoo.com.ar>
2023-05-18 14:26:14 -0300

    exercise 2.11 the phonebook step6 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
+  const [searchWord, setSearchWord] = useState('')
   const [newName, setNewName] = useState({
     name: '',
     number: '',
     id: ''
   })
 
-  const [searchWord, setSearchWord] = useState('')
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(res => setPersons(res.data))
+  }, [])
 
   const handleSearch = (e) => {
     setSearchWord(e.target.value)
