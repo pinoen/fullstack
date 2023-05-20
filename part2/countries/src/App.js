e6cde580e74d70139340ce5b8c3fa1abdbc5d91a
Emilio Pino <pinoen@yahoo.com.ar>
2023-05-19 23:36:53 -0300

    exercise 2.19 data for countries step2

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -17,11 +17,19 @@ function App() {
       })
   }, [country])
 
+  const handleShow = (name) => {
+    setCountriesData(countriesData.filter(country => country.name.common === name))
+  }
+
   return (
     <div>
       <label>find countries  <input value={country} onChange={handleChange} /></label>
       {countriesData.length > 10 && <p>Too many matches, specify another filter</p>}
-      {countriesData.length > 1 && countriesData.length <= 10 && countriesData.map(country => <p key={country.name.common}>{country.name.common}</p>)}
+      {countriesData.length > 1 && countriesData.length <= 10 && countriesData.map(country => (
+        <div key={country.name.common}>
+          <p>{country.name.common}</p><button onClick={() => handleShow(country.name.common)}>show</button>
+        </div>
+      ))}
       {countriesData.length === 1 &&
         <div>
           <h1>{countriesData[0].name.common}</h1>
@@ -29,7 +37,7 @@ function App() {
           <h3>Area: {countriesData[0].area}</h3>
           <h3>Languages:</h3>
           <ul>
-            {Object.values(countriesData[0].languages).map(lang => <li>{lang}</li>)}
+            {Object.values(countriesData[0].languages).map(lang => <li key={lang}>{lang}</li>)}
           </ul>
           <img alt={Object.values(countriesData[0].flags)[2]} src={Object.values(countriesData[0].flags)[0]} />
         </div>
