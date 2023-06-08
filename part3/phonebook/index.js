90c13ba10f40234f0ae5b955da3ee0c2ab82ac6a
Emilio Pino <pinoen@yahoo.com.ar>
2023-06-07 22:48:22 -0300

    exercise 3.17 done

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -87,6 +87,21 @@ app.delete('/api/persons/:id', (req, res, next) => {
   }).catch(err => next(err))
 })
 
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
+    })
+    .catch(err => next(err))
+})
+
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
