3e34526422ccb485721aaaacdd345eb926524f32
Emilio Pino <pinoen@yahoo.com.ar>
2023-06-07 22:36:17 -0300

    exercises 3.15 y 3.16 done

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -25,10 +25,10 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+app.use(express.static('build'))
 app.use(express.json())
 app.use(requestLogger)
 app.use(cors())
-app.use(express.static('build'))
 
 let persons = []
 
@@ -69,7 +69,7 @@ app.post('/api/persons/', (req, res) => {
   })
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id).then(person => {
     if (person) {
       res.json(person)
@@ -77,19 +77,28 @@ app.get('/api/persons/:id', (req, res) => {
       res.status(404).end()
     }
   }).catch(err => {
-    console.log(err)
-    res.status(400).send({ error: 'malformatted id' })
+    next(err)
   })
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id).then(result => {
+    res.status(204).end()
+  }).catch(err => next(err))
 })
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id!' })
+  }
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
